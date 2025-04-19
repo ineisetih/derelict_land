5984f5f016fcde1502da4b05966dece5b4ebd4e5
ineisetih <maksimad11pro@gmail.com>
2025-04-20 02:13:38 +0500

    minor changes

--- items/ItemGenerator.cpp
+++ items/ItemGenerator.cpp
@@ -4,15 +4,21 @@
 #include "Gems.h"
 #include <random>
 
+#define TEMPLATE_1 8.0f
+#define TEMPLATE_2 5.0f
+#define TEMPLATE_3 2.0f
+#define TEMPLATE_4 0
+
+
 std::shared_ptr<IItem> ItemGenerator::GenerateItem() {
   int choice = rand() % 3;
   switch (choice) {
     case 0:
-      return std::make_shared<Sword>("Dropped Sword", 8.0f);
+      return std::make_shared<Sword>("Dropped Sword", TEMPLATE_1);
     case 1:
-      return std::make_shared<Armor>("Dropped Armor", 5.0f, 0);
+      return std::make_shared<Armor>("Dropped Armor", TEMPLATE_2, TEMPLATE_4);
     case 2:
-      return std::make_shared<Gem>("Random Gem", 2.0f);
+      return std::make_shared<Gem>("Random Gem", TEMPLATE_3);
     default:
       return nullptr;
   }
